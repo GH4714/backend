e2282749ff255332f15d675216877ecbf994aa42
rjsgml4717 <rjsgml4717@gmail.com>
2023-07-11 16:41:33 +0900

    완성

---
+++ src/main/java/com/mysite/backend/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.mysite.backend.controller;
+
+import com.mysite.backend.model.User;
+import com.mysite.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
